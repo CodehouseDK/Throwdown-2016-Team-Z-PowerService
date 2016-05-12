b41b2254e45fd252786f58349aabf96ea91ed489
Per Manniche Bering <pmbering@gmail.com>
2016-05-12 20:01:05 +0200

    cleanup

--- src/PowerService/wwwroot/scripts/app.js
+++ src/PowerService/wwwroot/scripts/app.js
@@ -13,23 +13,24 @@ function init() {
 }
 
 function onOpen(evt) {
-    console.log("connected.");
+    console.log("connected", evt);
 }
 
 function onClose(evt) {
-    console.log("disconnected...");
+    console.log("disconnected...", evt);
 
     init();
 }
 
 function onError(evt) {
-    console.log("socket error")
+    console.log("socket error", evt)    
 }
 
 function onMessage(evt) {
     var data = JSON.parse(evt.data);
-    var html = "<h1>CPU Load: " + Math.round(data.AverageLoad) + "%</h1>";
+    var html = "";
 
+    html += "<h1>CPU load: " + Math.round(data.AverageLoad) + "%</h1>";
     html += "<h2>" + Math.round(data.AvailableMhz / 1000) + " Ghz available across " + data.AvailableCores + " cores on " + data.Computers + " workstations</h2>";
     html += "<h3>Updated: " + data.Timestamp + "</h3>";
 
